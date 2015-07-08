e8f569637421834c7b8af3dbe1ed45a592df9b90
Anthony Johnson <aj@ohess.org>
2015-07-07 23:19:43 -0700

    Add cross reference testing
    This adds mocking on calls/returns to domain find_obj. This allows for testing
    by writing inline references in test cases.
    
    There are several failing test cases that were surfaced with testing. These
    issues will be opened and resolved separately:
    
     * Non-nestable objects are not propagating the dn:prefix set in temporary data
     * Domain objects with colliding names but differing types are not separately
       addressable
    
    Fixes #6

--- tests/util.py
+++ tests/util.py
@@ -3,13 +3,15 @@ import os
 import shutil
 from textwrap import dedent
 
-from mock import patch
+from mock import Mock, patch
 
 from docutils.io import StringOutput
 
 from sphinx.builders.text import TextBuilder
 from sphinx.application import Sphinx
 from sphinx.environment import SphinxFileInput
+
+from sphinxcontrib.dotnetdomain import DotNetDomain
 
 
 class MockSphinx(Sphinx):
@@ -46,6 +48,29 @@ class MockTestBuilder(TextBuilder):
         self.output[docname] = self.writer.output
 
 
+class MockWithReturn(Mock):
+    '''Mock with return value tracking
+
+    Return values are stored in :py:attr:`call_return_list`, as a list of tuples.
+    Tuples consist of ``(call_params, return_value)``, where:
+
+        call_params
+            A list of call params to match using :py:meth:`Mock.assert_has_calls`
+
+        return_value
+            The return value from the call with ``call_params``
+    '''
+
+    def __init__(self, *args, **kwargs):
+        super(MockWithReturn, self).__init__(*args, **kwargs)
+        self.call_return_list = []
+
+    def __call__(self, *args, **kwargs):
+        ret = super(MockWithReturn, self).__call__(*args, **kwargs)
+        self.call_return_list.append((self.call_args, ret))
+        return ret
+
+
 class SphinxTestCase(unittest.TestCase):
     '''Test case that processes build input/output in memory for the most part
 
@@ -63,9 +88,15 @@ class SphinxTestCase(unittest.TestCase):
             buildername=None,
         )
 
+        self.patch = patch.object(DotNetDomain, 'find_obj',
+                                  wraps=self.app.env.domains['dn'].find_obj,
+                                  new_callable=MockWithReturn)
+        self.mocked_find_obj = self.patch.start()
+
     def tearDown(self):
         shutil.rmtree(os.path.join(os.path.dirname(__file__), 'fixtures',
                                    'example', '_build'))
+        self.patch.stop()
 
     def assertRef(self, refname, type_=None, docname=None, domain='dn'):
         '''Assert reference is found and matches criteria
@@ -95,3 +126,54 @@ class SphinxTestCase(unittest.TestCase):
             pass
         else:
             AssertionError('Reference match found')
+
+    def assertXRef(self, name, prefix=None, obj_type=None, doc='index',
+                   ret_name=None):
+        '''Assert mocked find_obj was called correctly
+
+        Test that inline references generate the correct calls and return to
+        :py:meth:`DotNetDomain.find_obj`. This requires testing data with inline
+        references.
+
+        :param name: Reference name
+        :param prefix: Reference prefix
+        :param obj_type: Reference type
+        :param doc: Document name as part of the reference return
+        :param ret_name: Name of object that is found during search, this
+                         defaults to ``<prefix>.<name>``, but may differ if
+                         doing a reverse search
+        '''
+        # Assert called correctly and return exists
+        args = (self.app.env, prefix, name, obj_type, 0)
+        self.mocked_find_obj.assert_has_calls(calls=[(args, {})])
+        found = None
+        for (mock_call, mock_return) in self.mocked_find_obj.call_return_list:
+            if mock_call == (args, {}):
+                found = mock_return
+        if found is None:
+            raise AssertionError('Return not found')
+
+        # Check return
+        if ret_name is None:
+            ret_name = name
+            if prefix is not None:
+                ret_name = '.'.join([prefix, name])
+        (found_name, found_meta) = found
+        self.assertIsNotNone(found_name,
+                             'XRef {0} not found'.format(ret_name))
+        (found_doc, found_type) = found_meta
+        self.assertEqual(found_name, ret_name)
+        self.assertEqual(found_doc, doc)
+        self.assertTrue(any(cls.short_name == obj_type
+                            for (long_name, cls)
+                            in self.app.env.domains['dn'].directives.items()
+                            if long_name == found_type))
+
+    def assertNoXRef(self, *args, **kwargs):
+        '''Inverse of :py:meth:`assertXRef`'''
+        try:
+            self.assertXRef(*args, **kwargs)
+        except AssertionError:
+            pass
+        else:
+            AssertionError('XRef match found')
